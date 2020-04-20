377eace9f8ead0ee1ec7c7e14f9d1fccd8c59f9b
Thanh Ha <zxiiro@gmail.com>
2020-04-20 11:04:29 -0400

    Fix publish not adding new files
    Signed-off-by: Thanh Ha <zxiiro@gmail.com>

--- publish.sh
+++ publish.sh
@@ -4,5 +4,6 @@ export PRE_COMMIT_ALLOW_NO_CONFIG=1
 
 cp .nojekyll /tmp/site
 cd /tmp/site
-git commit -asm "Update site $(date +'%Y%m%d-%H%M')"
+git add .
+git commit -sm "Update site $(date +'%Y%m%d-%H%M')"
 git push
